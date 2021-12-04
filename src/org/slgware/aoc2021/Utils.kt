b80eddbde6e0765ea9655322fa35fdcdf1b1c628
steven <steven@slgware.org>
2021-12-04 14:00:12 -0800

    day 1

--- src/org/slgware/aoc2021/Utils.kt
+++ src/org/slgware/aoc2021/Utils.kt
@@ -5,9 +5,14 @@ import java.math.BigInteger
 import java.security.MessageDigest
 
 /**
- * Reads lines from the given input txt file.
+ * Reads lines from the given input text file.
  */
 fun readInput( name: String ) = File( "src/org/slgware/aoc2021", "$name.text" ).readLines()
+
+/**
+ * Reads lines from the given input text file.
+ */
+fun readIntegers( name: String ) = readInput( name ).map( String::toInt )
 
 /**
  * Converts string to md5 hash.
--- src/org/slgware/aoc2021/day01/Implementation.kt
+++ src/org/slgware/aoc2021/day01/Implementation.kt
@@ -1,24 +1,44 @@
 package org.slgware.aoc2021.day01
 
-import org.slgware.aoc2021.readInput
+import org.slgware.aoc2021.readIntegers
 
 fun main()
 {
-    fun part1( input: List<String> ): Int
+    fun part1( input: Sequence<Int> ): Int
     {
-        return input.size
+        var accumulator = 0
+        var previous = Int.MAX_VALUE
+        input.forEach { n ->
+            if ( previous == Int.MAX_VALUE ) { /* no-op */ }
+            else if ( n > previous ) { ++accumulator }
+            previous = n
+        }
+        return accumulator
     }
 
-    fun part2( input: List<String> ): Int
+    fun triplets( raw: List<Int> ): List<Int>
     {
-        return input.size
+        val result = mutableListOf<Int>()
+        var index = 0
+        while ( index + 2 <= raw.lastIndex )
+        {
+            result.add (raw[index] + raw[index+1] + raw[index+2] )
+            ++index
+        }
+        return result
+    }
+
+    fun part2( input: List<Int> ): Int
+    {
+        return part1( triplets( input ).asSequence() )
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput( "day01/test_input" )
-    check( part1( testInput ) == 1 )
+    val testInput = readIntegers( "day01/test_input" )
+    check( part1( testInput.asSequence() ) == 7 )
+    check( part2( testInput ) == 5 )
 
-    val input = readInput( "day01/input" )
-    println( part1( input ) )
+    val input = readIntegers( "day01/input" )
+    println( part1( input.asSequence() ) )
     println( part2( input ) )
 }
